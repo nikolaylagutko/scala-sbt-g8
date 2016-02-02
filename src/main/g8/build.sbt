42567b9b27cdc5e71cd39ce8047d5710881ad058
Nikolay Lagutko <nikolay.lagutko@mail.com>
2016-02-02 22:42:50 +0300

    template for logging configuration

--- src/main/g8/build.sbt
+++ src/main/g8/build.sbt
@@ -10,12 +10,15 @@ version := "$version$"
 scalaVersion := Versions.scala
 
 libraryDependencies ++= Seq(
-
+  //compile dependencies
+  scala_logging,
 
   //test dependencies
   specs2.core,
   specs2.scalacheck,
-  specs2.mock
+  specs2.mock,
+
+  logback
 )
 
 dependencyOverrides ++= Set(
--- src/main/g8/project/Dependencies.scala
+++ src/main/g8/project/Dependencies.scala
@@ -4,8 +4,10 @@ import sbt._
 
 object Versions {
 
-  val scala  = "$scala$"
-  val specs2 = "$specs2$"
+  val scala         = "$scala$"
+  val specs2        = "$specs2$"
+  val scala_logging = "$scala_logging$"
+  val logback       = "$logback$"
 
 }
 
@@ -16,5 +18,9 @@ object Dependencies {
     val scalacheck = "org.specs2" %% "specs2-scalacheck" % Versions.specs2 % "test"
     val mock       = "org.specs2" %% "specs2-mock"       % Versions.specs2 % "test"
   }
+
+  val scala_logging = "com.typesafe.scala-logging" %% "scala-logging" % Versions.scala_logging
+
+  val logback = "ch.qos.logback" % "logback-classic" % Versions.logback % "test"
 
 }
